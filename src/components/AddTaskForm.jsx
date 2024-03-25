766e551013266645a6e32c643734a56aac315661
DipaliTalware <d.talware@gmail.com>
2024-03-25 11:27:16 +0100

    Todo list with ReactJS

--- src/components/AddTaskForm.jsx
+++ src/components/AddTaskForm.jsx
@@ -1,11 +1,47 @@
-import React from 'react'
+import { useState } from "react"
 
-const AddTaskForm = () => {
+const AddTaskForm = ({onSubmit}) => {
+  const[addTask, setAddTask] = useState({taskName :"", deadline:"", priority:""})
+
+  function taskInput(e){
+    const {name, value} = e.target;
+    setAddTask({...addTask, [name]:value})
+  }
+
+  const formSubmit = (event) => {
+    event.preventDefault();
+    onSubmit(addTask);
+    setAddTask({ taskName: "", deadline: "", priority: "" });
+  }
+
   return (
-    <div>
-        <form action="" className='flex justify-center mt-10'>
-        <input type="text" placeholder='Add new task' className='border-2 mr-4 px-5 py-2 w-80'/>
-            <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded' >Add</button>
+    <div> 
+        <h1 className="text-white text-center text-3xl mt-10">ToDo List</h1>
+        <form action="" className='flex justify-center mt-4' onSubmit={formSubmit}>
+            <div className="">
+                <input type="text" name="taskName" value={addTask.taskName} placeholder='Add new task' className='border-2 px-4 py-3 mt-5 mr-4 w-80 h-11 border-teal-500 bg-grey-200 text-black' onChange={taskInput}/>
+            </div>
+            <div >
+              <input 
+                  type="Date"  
+                  name="deadline" 
+                  value= {addTask.deadline} 
+                  className="text-gray-400 px-4 py-3 mt-5 border-2 border-teal-500 h-11" 
+                  onChange={taskInput}
+              />
+            </div>
+            <div className="flex flex-col px-10">
+                <select name="priority" value={addTask.priority} className="h-11 mt-5 border-2 border-teal-500" onChange={taskInput}>
+                  <option disabled value="">Select priority</option>
+                  <option value="priority: low">Low</option>
+                  <option value="priority: medium">Medium</option>
+                  <option value="priority: high">High</option>
+                </select>
+            </div>
+            <div>
+              <button className='bg-teal-500 hover:bg-teal-600 text-white font-bold px-4 py-2 mt-5 rounded w-20' >Add
+              </button>
+            </div>
         </form>
     </div>
   )
--- src/components/TaskTabs.jsx
+++ src/components/TaskTabs.jsx
@@ -1,13 +1,29 @@
-import React from 'react'
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 
 const TaskTabs = () => {
   return (
-    <div className='flex justify-center mt-10'>
-        <button className='px-5'>All Task</button>
-        <button className='px-5'>Active Task</button>
-        <button className='px-5'>Completed Task</button>
-    </div>
+        <Tabs>
+          <TabList > 
+            <Tab>All Task</Tab>
+            <Tab>Active Task</Tab>
+            <Tab>Completed Task</Tab>
+          </TabList>
+      
+          <TabPanel>
+            <h2>Any content 1</h2>
+          </TabPanel>
+          <TabPanel>
+            <h2>Any content 2</h2>
+          </TabPanel>
+      </Tabs>
   )
+    // <div className='flex justify-center mt-10'>
+    //     <button className='px-5'>All Task</button>
+    //     <button className='px-5'>Active Task</button>
+    //     <button className='px-5'>Completed Task</button>
+    // </div>
+
 }
 
 export default TaskTabs
